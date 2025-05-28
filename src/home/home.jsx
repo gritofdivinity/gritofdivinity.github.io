ea009a1854056992e982ceabb91bddf391404378
gritofdivinity <gritofdivinity@gmail.com>
2025-05-28 16:12:23 +1200

    fix img url

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -3,7 +3,7 @@ import './home.css'
 export default 
 function Home() { return (
 <div id="home">
-    <div id='imgwrap'><img src='../../public/ai-samplescene.webp'/></div>
+    <div id='imgwrap'><img src='../public/ai-samplescene.webp'/></div>
     <p id='p1'>Before the judgement of the world, what if... zombies</p>
     <p id='p2'>Sample scene, made by AI</p>
 
