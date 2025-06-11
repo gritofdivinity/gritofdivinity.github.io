16f73184861eea327a4265e97b56752ae5d92937
gritofdivinity <gritofdivinity@gmail.com>
2025-06-11 12:32:43 +1200

    June 11 - new layout

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -4,8 +4,8 @@ export default
 function Home() { return (
 <div id="home">
     <div id='imgwrap'><img src='/ai-samplescene.webp'/></div>
-    <p id='p1'>Before the judgement of the world, what if... zombies</p>
-    <p id='p2'>Sample scene, made by AI</p>
+    <p id='p1'>In the judgement of the world. . . what if. . . zombies</p>
+    <p id='p2'>A young teen recieves a vision, warning him of an incoming disaster.</p>
 
 </div>
 )}
